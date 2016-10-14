faef6a40ed375e8a01a98afad2542415d6d1ffa4
Bmar21 <brandonmalor21@gmail.com>
2016-10-14 12:47:51 -0600

    made chatbot

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,10 @@
+package chat.controller;
+
+public class ChatbotRunner 
+{
+	public static void main (String [] args)
+	{
+		ChatbotController ChatbotAppController = new ChatbotController();
+		ChatbotAppController.start();
+	}
+}
